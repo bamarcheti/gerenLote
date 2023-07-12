bc60655d0d36872fa3c44338d1e43058e3d3d2a6
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-07-12 17:49:16 -0400

    style[web]: Adicionando componentes de cores

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#0F766E',
+        secondary: '#3A36DB',
+        textA: '#06152B',
+        textB: '#272727',
+        textC: '#767676',
+        default: '#9ca3af',
+        clean: '#e5e7eb',
+        error: '#C73658'
+      }
+    }
   },
   plugins: []
 };
