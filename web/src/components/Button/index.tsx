ca96120a4136374e709ba3a7b3a392e3e4fb516c
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-07-12 17:48:36 -0400

    feat[web]: Criando componente Button

---
+++ web/src/components/Button/index.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+
+interface Props {
+  onClick?: () => void;
+  type?: 'submit' | 'button' | 'reset';
+  variant?: 'primary' | 'secondary' | 'default' | 'cancel' | 'clean' | 'error';
+  buttonText?: string;
+  children?: React.ReactNode;
+  styles?: string;
+  className?: string;
+  style?: any;
+}
+
+const Button: React.FC<Props> = ({
+  children,
+  buttonText,
+  onClick,
+  styles,
+  type,
+  variant = 'default',
+  className,
+  style
+}) => {
+  return (
+    <button
+      style={style}
+      className={
+        `${variant} transition duration-300 hover:bg-opacity-90 font-medium rounded text-base px-9 py-1 text-center block ${
+          styles ? styles : null
+        }` + className
+      }
+      type={type}
+      onClick={onClick}
+    >
+      {children || buttonText}
+    </button>
+  );
+};
+
+export default Button;
