3ede5400c5a2ec8f54128b804bf075c9071dc253
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-07-15 02:46:14 -0400

    fix[web]: Alterando estilização do MainLayout

--- web/src/components/MainLayoutAuth/index.tsx
+++ web/src/components/MainLayoutAuth/index.tsx
@@ -1,11 +1,12 @@
 interface Props {
+  className?: string;
   children: React.ReactNode;
 }
 
-const MainLayoutAuth: React.FC<Props> = ({ children }) => {
+const MainLayoutAuth: React.FC<Props> = ({ className, children }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen overflow-y-auto bg-primary px-[365px] py-36">
-      <div className="bg-white rounded-md flex flex-col items-center justify-center gap-4 w-full h-full p-[70px]">
+      <div className="bg-white rounded-md flex flex-col items-center justify-center gap-4 w-full h-full">
         {children}
       </div>
     </div>
