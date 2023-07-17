ec27a0e2e7e4f86da42960108c185f8fd8063896
Lucas Almeida Tiburtino da Silva <64665422+LucasATS@users.noreply.github.com>
2023-07-17 11:54:38 -0400

    style[web] Alteração do pedding do painel Auth

--- web/src/components/MainLayoutAuth/index.tsx
+++ web/src/components/MainLayoutAuth/index.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const MainLayoutAuth: React.FC<Props> = ({ className, children }) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen overflow-y-auto bg-primary px-[365px] py-36">
+    <div className="flex flex-col items-center justify-center w-full h-screen overflow-y-auto bg-primary px-[365px] py-20">
       <div className="bg-white rounded-md flex flex-col items-center justify-center gap-4 w-full h-full">
         {children}
       </div>
